dfdb1f698d1004e085831e8a84ba042e3c998b99
igorskyflyer <igor.dvlpr@gmail.com>
2022-08-27 20:02:12 +0200

    Add maxIncreasePatch() method

--- src/index.js
+++ src/index.js
@@ -400,6 +400,16 @@ class Keppo {
   maxIncreaseMinor() {
     return Number.MAX_SAFE_INTEGER - this.minor
   }
+
+  /**
+   * Returns the maximum possible value that can be used to increase the patch version number.
+   *
+   * Read more about Integer safety on {@link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number/isSafeInteger MDN}.
+   * @returns {number}
+   */
+  maxIncreasePatch() {
+    return Number.MAX_SAFE_INTEGER - this.patch
+  }
 }
 
 /**
